75965f05c9c0de958d041f48843f00db7a7fce91
attila <ngonella@calpoly.edu>
2018-06-07 10:38:03 -0700

    Add in PS object into networking

--- networking.py
+++ networking.py
@@ -2,20 +2,22 @@ from __future__ import print_function
 
 import asyncore, asynchat, enum, json, logging, socket
 from ConfigParser import ConfigParser
+from policy_server import PS
 
 class PolicyServer(asyncore.dispatcher):
 
-    def __init__(self):
+    def __init__(self, ps_logic):
 	asyncore.dispatcher.__init__(self)
 
 	self.logger = logging.getLogger(self.__class__.__name__)
         self.handler = GenericHandler
+        self.ps_logic = ps_logic
 
 
     def handle_accept(self):
         connection, address = self.accept()
         self.logger.debug('accept -> %s', address)
-        self.handler(connection)
+        self.handler(connection, self.ps_logic)
         
 
     def handle_close(self):
@@ -32,11 +34,13 @@ class PolicyServer(asyncore.dispatcher):
 
 class GenericHandler(asynchat.async_chat):
 
-    def __init__(self, sock, terminator):
+    def __init__(self, sock, ps_logic, terminator):
 	asynchat.async_chat.__init__(self, sock=sock)
 
 	self.logger = logging.getLogger(self.__class__.__name__)
         self.buffer = []
+        self.ps_logic = ps_logic
+
         self.set_terminator(terminator)
 
     def collect_incoming_data(self, data):
@@ -44,6 +48,12 @@ class GenericHandler(asynchat.async_chat):
                           len(data),
                           data)
         self.buffer.append(data)
+
+    def _log_failure(self, e, msg):
+        self.logger.error(msg)
+        self.logger.debug(str(e))
+        self.logger.debug(msg)
+        self.close()
 
 
 class JsonProtocolType(enum.Enum):
@@ -55,30 +65,21 @@ class JsonProtocolType(enum.Enum):
 
 class JsonHandler(GenericHandler):
 
-    def __init__(self, sock):
-        GenericHandler.__init__(self, sock, '\n')
+    def __init__(self, sock, ps_logic):
+        GenericHandler.__init__(self, sock, ps_logic, '\n')
 
-    def _log_failure(self, e, msg):
-        self.logger.error(msg)
-        self.logger.debug(str(e))
-        self.logger.debug(msg)
-        self.close()
-
-    def found_terminator(self):
-        msg = ''.join(self.buffer)
-        self.buffer = []
-
+    def _parse_message(self, msg):
         try:
             json_dict = json.loads(msg)
         except ValueError as e:
             self._log_failure("Failed to parse JSON message")
-            return
+            return None
 
         try:
             message_type = JsonProtocolType[json_dict['type']]
         except KeyError as e:
             self._log_failure("Invalid message type")
-            return
+            return None
 
         data_field = str(message_type.name).lower() + "List"
 
@@ -86,21 +87,55 @@ class JsonHandler(GenericHandler):
             data = json_dict[data_field]
         except KeyError as e:
             self._log_failure("Could not get data field for message")
-            return
+            return None
 
         self.logger.debug('succesfully parsed json\n"""%s"""\n"""%s"""',
                           message_type,
                           data_field)
 
+        return message_type, data_field
+
+
+    def found_terminator(self):
+        msg = ''.join(self.buffer)
+        self.buffer = []
+
+        data = self._parse_message(msg)
+
+        if data is None:
+            return
+
+        message_type, data_field = data
+
+        if message_type == JsonProtocolType.INIT:
+            pass
+        elif message_type == JsonProtocolType.TR:
+            # resps = self.ps_logic.handle_request
+            pass
+        elif message_type == JsonProtocolType.RESP:
+            pass
+        elif message_type == JsonProtocolType.GS:
+            # resps = self.ps_logic.fwd_stripped_gs_metadata(data_field)
+            pass
+        elif message_type == JsonProtocolType.CANCEL:
+            pass
+
 
 class LcmHandler(GenericHandler):
-    pass
 
+    def __init__(self, sock, ps_logic):
+        GenericHandler.__init__(self, sock, ps_logic, 0)
+
+    def found_terminator(self):
+        msg = ''.join(self.buffer)
+        self.buffer = []
+
+        self.logger.debug('%s', msg)
 
 class JsonPolicyServer(PolicyServer):
 
-    def __init__(self, config):
-        PolicyServer.__init__(self)
+    def __init__(self, config, ps_logic):
+        PolicyServer.__init__(self, ps_logic)
 
         self.handler = JsonHandler
 
@@ -111,8 +146,8 @@ class JsonPolicyServer(PolicyServer):
 
 class LcmPolicyServer(PolicyServer):
 
-    def __init__(self, config):
-        PolicyServer.__init__(self)
+    def __init__(self, config, ps_logic):
+        PolicyServer.__init__(self, ps_logic)
 
         self.handler = LcmHandler
 
@@ -125,11 +160,13 @@ def main():
     config = ConfigParser()
     config.read('config.ini')
 
+    ps_logic = PS()
+
     logging.basicConfig(level=logging.DEBUG, 
             format='%(name)s: %(levelname)s: %(message)s')
 
-    JsonPolicyServer(config)
-    LcmPolicyServer(config)
+    JsonPolicyServer(config, ps_logic)
+    LcmPolicyServer(config, ps_logic)
 
     asyncore.loop()
 
